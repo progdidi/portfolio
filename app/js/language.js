2f12ca4ca63f6bbea22e785e2b5fd6e3200e33bc
Sasnouskaya Dziyana <progdidi@gmail.com>
2023-05-03 13:58:44 +0200

    project structure

---
+++ app/js/language.js
@@ -0,0 +1,12 @@
+const languageType = {
+    russian: {
+      title: 'Привет, я Диана Сосновская',
+    },
+
+    english: {
+      title: 'Hello, I am Diana Sosnovskaya'
+    }
+}
+
+export {languageType};
+
--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,20 @@
+import {languageType} from './language.js';
+
 $(function(){
-   
+    //multilingual
+    const langs = document.querySelector('.languages');
+    const change = document.querySelectorAll('.languages__item');
+
+    let title = document.querySelector('.promo__info-title');
+
+    change.forEach(elem => {
+      elem.addEventListener('click', () => {
+          langs.querySelector(".active").classList.remove("active")
+          elem.classList.add("active")
+          let attr = elem.getAttribute("language")
+          title.textContent = languageType[attr].title
+      })
+    })
+    
+    
 });
