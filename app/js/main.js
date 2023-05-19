9164dba440fde618913c76f03dea6443d4802c5f
Diana Sosnovskaya <progdidi@gmail.com>
2023-05-19 15:18:32 +0200

    works section

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,8 @@
-import {languageType} from './language.js';
 
 $(function(){
     //multilingual
-    const langs = document.querySelector('.languages');
-    const change = document.querySelectorAll('.languages__item');
 
-    let title = document.querySelector('.promo__info-title');
-
-    change.forEach(elem => {
-      elem.addEventListener('click', () => {
-          langs.querySelector(".active").classList.remove("active")
-          elem.classList.add("active")
-          let attr = elem.getAttribute("language")
-          title.textContent = languageType[attr].title
-      })
-    })
+    new WOW().init();
     
     
 });
