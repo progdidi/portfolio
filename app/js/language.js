228adfbc53e2a5cfe7998af9ef19eff17771ec89
Diana Sosnovskaya <progdidi@gmail.com>
2023-05-23 19:27:40 +0200

    language settings

--- app/js/language.js
+++ app/js/language.js
@@ -1,11 +1,8 @@
 const languageType = {
-    russian: {
-      title: 'Привет, я Диана Сосновская',
-    },
-
-    english: {
-      title: 'Hello, I am Diana Sosnovskaya'
-    }
+   title: {
+    ru: 'Привет, я Диана Сосновская',
+    eng: 'Hello, I am Diana Sosnovskaya'
+   }
 }
 
 export {languageType};
--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,35 @@
+const languageType = {
+    title: {
+     ru: 'Привет, я Диана Сосновская',
+     eng: 'Hello, I am Diana Sosnovskaya'
+    }
+ }
 
 $(function(){
     //multilingual
 
     new WOW().init();
+
+    //languages
+    
+
+    function setLang() {
+        for (let key in languageType) {
+            let elem = document.querySelector('.lng-' + key);
+            if (elem) {
+                
+              elem.innerHTML = languageType[key][this.value];
+            }
+          }
+    }
+    const langBtn = document.querySelector('.languages-btn');
+    langBtn.addEventListener('click', () => {
+        setLang()}
+        );
+
+        
+
+
     
     
 });
