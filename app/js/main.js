fc21f97f805c3c648a1feb1c612eb96fa40257ec
Diana Sosnovskaya <progdidi@gmail.com>
2023-05-24 13:34:36 +0200

    fixed header

--- app/js/main.js
+++ app/js/main.js
@@ -5,31 +5,55 @@ const languageType = {
     }
  }
 
-$(function(){
-    //multilingual
 
+$(function(){
+    //animation
     new WOW().init();
 
     //languages
-    
+    // function setLang() {
+    //     for (let key in languageType) {
+    //         console.log(key)
+    //         let elem = document.querySelector('.lng-' + key);
+    //         console.log(elem)
+    //         if (elem) {
+            
+    //           elem.innerHTML = languageType[key];
+    //         }
+    //       }
+    // }
+    // const langBtn = document.querySelector('.languages-btn');
+    // langBtn.addEventListener('click', () => {
+    //     setLang()}
+    //     );
 
-    function setLang() {
-        for (let key in languageType) {
-            let elem = document.querySelector('.lng-' + key);
-            if (elem) {
-                
-              elem.innerHTML = languageType[key][this.value];
-            }
-          }
-    }
-    const langBtn = document.querySelector('.languages-btn');
-    langBtn.addEventListener('click', () => {
-        setLang()}
-        );
 
+    //hover
+    const hoverItems = document.querySelectorAll('.works__item');
+    hoverItems.forEach((item) => {
+      item.addEventListener('mouseover', () => {
+        item.style.backgroundColor = "red"; 
+      })
+      item.addEventListener('mouseout', () => {
+        item.style.transform = "";
+      })
+    })
+
+    //scroll
+    const header = document.querySelector('.header');
+    const scrollHeight = window.pageYOffset;
+    const headerHeight = header.getBoundingClientRect().height;
+
+    window.addEventListener('scroll', () => {
+      if(scrollHeight > headerHeight) {
         
-
-
+        header.classList.add('fixed-header')
+      } else {
+        console.log(scrollHeight)
+        header.classList.remove('fixed-header')
+      }
+    })
+        
     
     
 });
